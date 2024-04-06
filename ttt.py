f711795390d0abc1ae7ca3ce2272c2474dc8bb7d
Justin <122790149+jquagga@users.noreply.github.com>
2024-04-06 13:49:52 -0400

    Update ttt.py
    Fixing AI breakage of AI program. Recursion!

--- ttt.py
+++ ttt.py
@@ -166,14 +166,12 @@ def send_notifications(calljson, destinations):
 
 def import_notification_destinations():
     import csv
-
-with open("destinations.csv", mode="r") as inp:
-    reader = csv.reader(inp)
-    destinations = {}
-    for rows in reader:
-        if len(rows) >= 2:
-            destinations[rows[0]] = rows[1]
-
+    with open("destinations.csv", mode="r") as inp:
+        reader = csv.reader(inp)
+        destinations = {}
+        for rows in reader:
+            if len(rows) >= 2:
+                destinations[rows[0]] = rows[1]
 
 def main():
     destinations = import_notification_destinations()
