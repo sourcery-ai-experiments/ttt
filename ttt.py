aff36719416cc6e0c53bbdce5aa0009774d23820
Justin <122790149+jquagga@users.noreply.github.com>
2024-04-06 13:19:15 -0400

    Update ttt.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- ttt.py
+++ ttt.py
@@ -167,11 +167,12 @@ def send_notifications(calljson, destinations):
 def import_notification_destinations():
     import csv
 
-    with open("destinations.csv", mode="r") as inp:
-        reader = csv.reader(inp)
-        destinations = {rows[0]: rows[1] for rows in reader}
-
-    return destinations
+with open("destinations.csv", mode="r") as inp:
+    reader = csv.reader(inp)
+    destinations = {}
+    for rows in reader:
+        if len(rows) >= 2:
+            destinations[rows[0]] = rows[1]
 
 
 def main():
