551513beb4d77ed8805663a4a7e35904789be2d4
Justin Settle <jus10@partlycloudy.org>
2024-04-11 19:42:33 -0400

    Transformer model selections via ENV

--- ttt.py
+++ ttt.py
@@ -130,7 +130,9 @@ def transcribe_transformers(calljson, audiofile):
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-    model_id = "distil-whisper/distil-large-v3"
+    model_id = os.environ.get(
+        "TTT_TRANSFORMERS_MODEL_ID", "distil-whisper/distil-large-v3"
+    )
 
     model = AutoModelForSpeechSeq2Seq.from_pretrained(
         model_id,
